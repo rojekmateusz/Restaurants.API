034fcb3669844f90bed8d9020b479c8e63aa351a
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-02-17 10:26:47 +0100

    add project modules and entities in domain

--- Restaurants.Domain/obj/Debug/net8.0/Restaurants.Domain.AssemblyInfo.cs
+++ Restaurants.Domain/obj/Debug/net8.0/Restaurants.Domain.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.API")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0667d2ca6e5b29e576c5d82c64ed095a74f6a1")]
-[assembly: System.Reflection.AssemblyProductAttribute("Restaurants.API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.API")]
+[assembly: System.Reflection.AssemblyProductAttribute("Restaurants.Domain")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
